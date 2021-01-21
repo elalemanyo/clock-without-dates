9af16dbf27a7ea8e09a495647bda8d707fc1f805
victor <victor@elalemanyo.de>
2021-01-21 21:40:23 +0100

    init repo

---
+++ clock.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# Clock returns a string with the given hours and minutes
+class Clock
+  attr_accessor :hour, :minute
+
+  def initialize(hour: 0, minute: 0)
+    @hour = hour
+    @minute = minute
+  end
+
+  def to_s
+    seconds = ((hour * 60) + minute) * 60
+    Time.at(seconds).utc.strftime("%H:%M")
+  end
+
+  def + (clock)
+    hour = self.hour + clock.hour
+    minute = self.minute + clock.minute
+    return Clock.new(hour: hour, minute: minute)
+  end
+
+  def - (clock)
+    hour = self.hour - clock.hour
+    minute = self.minute - clock.minute
+    return Clock.new(hour: hour, minute: minute)
+  end
+
+  def == (clock)
+    clock1 = Clock.new(hour: self.hour, minute: self.minute)
+    clock2 = Clock.new(hour: clock.hour, minute: clock.minute)
+    clock1.to_s == clock2.to_s
+  end
+end
